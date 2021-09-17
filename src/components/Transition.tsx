f2deb8b6ae567953bfe2343ceea38386744ff271
factal <factalmusic@gmail.com>
2021-09-18 00:29:47 +0900

    fix transition

--- src/components/Transition.tsx
+++ src/components/Transition.tsx
@@ -1,51 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group'
 
 type Props = {
   location: string
 }
 
-const TIMEOUT: number = 150
+const TIMEOUT: number = 100
 
 const getTransitionStyles = {
   entering: {
-    position: `fixed`,
+    //position: 'fixed',
+    //transition: `opacity ${TIMEOUT}ms ease-in-out`,
     opacity: 0,
-    //transform: `translateX(50px)`,
   },
   entered: {
-    transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
+    transition: `opacity ${TIMEOUT}ms ease-in-out`,
     opacity: 1,
-    //transform: `translateX(0px)`,
-    //animation: "blink .2s linear 2",
+    animation: "blink .2s linear 2",
   },
   exiting: {
-    transition: `opacity ${TIMEOUT}ms ease-in-out, transform ${TIMEOUT}ms ease-in-out`,
     opacity: 0,
-    //transform: `translateX(-50px)`,
   },
 }
 
 const Transition: React.FC<Props> = (props) => {
+
+  useEffect(() => {
+    const xpos = document.documentElement.scrollLeft || document.body.scrollLeft;
+    const ypos = document.documentElement.scrollTop || document.body.scrollTop;
+  })
+  
+
   return (
-    <TransitionGroup style={{ position: 'relative' }}>
-      <ReactTransition
-        key={props.location}
-        timeout={{
-          enter: TIMEOUT,
-          exit: TIMEOUT,
+    <TransitionGroup>
+      <ReactTransition key={props.location} timeout={{ enter: TIMEOUT }}>
+        { (status) => {
+          return (
+            <div id={props.location}
+              style={{
+                opacity: 1,
+                margin: 0,
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                //@ts-ignore
+                ...getTransitionStyles[status],
+              }}
+            >
+              {props.children}
+            </div>
+          )
         }}
-      >
-        { (status) => (
-          <div
-            style={{
-              //@ts-ignore
-              ...getTransitionStyles[status],
-            }}
-          >
-            {props.children}
-          </div>
-        )}
       </ReactTransition>
     </TransitionGroup>
   )
